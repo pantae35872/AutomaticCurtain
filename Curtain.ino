29943bd463777e7e6303e6a50a90586aadf69b5e
Pantae <pantae35872@gmail.com>
2024-11-01 02:45:47 +0700

    Not using delay

--- Curtain.ino
+++ Curtain.ino
@@ -52,49 +52,55 @@ enum class CurtainState {
   Close
 };
 
+enum class MotorDirection {
+  Right,
+  Left,
+};
+
 // ---- MACRO ----
 #define DEFINE_VECTOR(Type, Name)\
     Type Name##_array[ELEMENT_COUNT_MAX];  \
     Vector<Type> Name;           \
     Name.setStorage(Name##_array);
 
-// ---- DEFINITION ----
+// ---- PINS ----
 
 #define CLOCK_INTERRUPT_PIN 2
 #define MOTOR_R_PWM_PIN 5 
 #define MOTOR_L_PWM_PIN 6 
-#define BUTTON_1_PIN 3
-#define BUTTON_2_PIN 4
+#define BUTTON_OPEN_CLOSE_PIN 3
+#define BUTTON_BUTTON2 4
 #define BUTTON_3_PIN A0
 #define BUTTON_4_PIN A1
 #define BUTTON_5_PIN 7
 #define BUTTON_6_PIN 8
 #define BUTTON_7_PIN 9
 #define BUTTON_8_PIN 10
-#define BUTTON_9_PIN 11
+#define CHANGE_TIME_BUTTON_PIN 11
 
 // ---- GLOBAL VARIABLES ----
 
 CurtainState curtainState = CurtainState::Open;
 CurtainState lastCurtainState = CurtainState::Close;
-ToggleButton openCloseButton(BUTTON_1_PIN);
-Button button2(BUTTON_2_PIN);
+ToggleButton openCloseButton(BUTTON_OPEN_CLOSE_PIN);
+Button button2(BUTTON_BUTTON2);
 Button button3(BUTTON_3_PIN);
 Button button4(BUTTON_4_PIN);
 Button button5(BUTTON_5_PIN);
 Button button6(BUTTON_6_PIN);
 Button button7(BUTTON_7_PIN);
 Button button8(BUTTON_8_PIN);
-Button button9(BUTTON_9_PIN);
+Button setCurrentTimeButton(CHANGE_TIME_BUTTON_PIN);
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 RTC_DS3231 rtc;
 
 bool alarm = false;
-bool opening = false;
-bool open = false;
-const double EPSILON = 1e-6;
-const int ELEMENT_COUNT_MAX = 4;
+
+bool motor_busy = false;
+uint32_t motor_issued_time = 0;
+int motor_wait_time = 0;
+MotorDirection motor_direction = MotorDirection::Right;
 
 
 // ---- UTILS ----
@@ -136,15 +142,18 @@ void setup_motor() {
 }
 
 void setup_button() {
-  pinMode(BUTTON_1_PIN, INPUT);
-  pinMode(BUTTON_2_PIN, INPUT);
+  pinMode(BUTTON_OPEN_CLOSE_PIN
+, INPUT);
+  pinMode(BUTTON_BUTTON2
+
+, INPUT);
   pinMode(BUTTON_3_PIN, INPUT);
   pinMode(BUTTON_4_PIN, INPUT);
   pinMode(BUTTON_5_PIN, INPUT);
   pinMode(BUTTON_6_PIN, INPUT);
   pinMode(BUTTON_7_PIN, INPUT);
   pinMode(BUTTON_8_PIN, INPUT);
-  pinMode(BUTTON_9_PIN, INPUT);
+  pinMode(CHANGE_TIME_BUTTON_PIN, INPUT);
 }
 
 void setup_rtc() {
@@ -210,117 +219,41 @@ void display_state() {
   }
 }
 
-// ---- 24 GAME SLOVER ----
-int calculatons = 0;
-
-void solve24Game(Vector<double>& numbers, double target) {
-  calculatons = 0;
-  DEFINE_VECTOR(String, expressions);
-  for (auto& n : numbers) {
-    expressions.push_back(String((int)n));
-  }
-
-  lcd.setCursor(0, 2);
-  lcd << "Calculating...";
-  if (!solve24GameHelper(numbers, expressions, target)) {
-    lcd.setCursor(0, 2);
-    lcd << "No solution found.";
-    lcd.setCursor(0, 3);
-    clear_line(3);
-  }
-}
-
-bool solve24GameHelper(Vector<double>& numbers, Vector<String>& expressions, double target) {
-    
-    if (numbers.size() == 1) {
-        if (fabs(numbers[0] - target) < EPSILON) {
-            Serial << "Solution: " << expressions[0] << endl;
-            lcd.setCursor(0, 2);
-            clear_line(2);
-            lcd << "Solution: ";
-            lcd.setCursor(0, 3);
-            lcd << expressions[0];
-            return true;
-        }
-        return false;
-    }
-
-    for (int i = 0; i < numbers.size(); ++i) {
-        for (int j = 0; j < numbers.size(); ++j) {
-            if (i != j) {
-                DEFINE_VECTOR(double, nextNumbers);
-                DEFINE_VECTOR(String, nextExpressions);
-
-                for (int k = 0; k < numbers.size(); ++k) {
-                    if (k != i && k != j) {
-                        nextNumbers.push_back(numbers[k]);
-                        nextExpressions.push_back(expressions[k]);
-                    }
-                }
-
-                struct Result {
-                    double value;
-                    String expression;
-                };
-                DEFINE_VECTOR(Result, results);
-                results.push_back({numbers[i] + numbers[j], "(" + expressions[i] + " + " + expressions[j] + ")"});
-                results.push_back({numbers[i] - numbers[j], "(" + expressions[i] + " - " + expressions[j] + ")"});
-                results.push_back({numbers[j] - numbers[i], "(" + expressions[j] + " - " + expressions[i] + ")"});
-                results.push_back({numbers[i] * numbers[j], "(" + expressions[i] + " * " + expressions[j] + ")"});
-
-                if (fabs(numbers[j]) > EPSILON) {
-                    results.push_back({numbers[i] / numbers[j], "(" + expressions[i] + " / " + expressions[j] + ")"});
-                }
-                if (fabs(numbers[i]) > EPSILON) {
-                    results.push_back({numbers[j] / numbers[i], "(" + expressions[j] + " / " + expressions[i] + ")"});
-                }
-                lcd.setCursor(0, 3);
-                calculatons += results.size();
-                if (calculatons % 100 == 0) { 
-                  lcd << calculatons << " Calculations.";
-                }
-                for (int r = 0; r < results.size(); ++r) {
-                    nextNumbers.push_back(results[r].value);
-                    nextExpressions.push_back(results[r].expression);
-
-                    if (solve24GameHelper(nextNumbers, nextExpressions, target)) {
-                        return true;
-                    }
-
-                    nextNumbers.pop_back();
-                    nextExpressions.pop_back();
-                }
-            }
-        }
-    }
-    return false;
-}
-
 // ---- MOTOR CONTROLLER --- 
 
-void motor_stop() {
-  analogWrite(MOTOR_R_PWM_PIN, 0);
-  analogWrite(MOTOR_L_PWM_PIN, 0);
+void issue_motor(MotorDirection direction, int time) {
+  motor_issued_time = rtc.now().unixtime();
+  motor_direction = direction;
+  motor_wait_time = time;
+  motor_busy = true;
 }
 
-void motor_right(int time) {
-  analogWrite(MOTOR_R_PWM_PIN, 128);
-  analogWrite(MOTOR_L_PWM_PIN, 0);
-  delay(time);
-  motor_stop();
-}
-
-void motor_left(int time) {
-  analogWrite(MOTOR_R_PWM_PIN, 0);
-  analogWrite(MOTOR_L_PWM_PIN, 128);
-  delay(time);
-  motor_stop();
+void motor_update() {
+  if (motor_busy) {
+    if (rtc.now().unixtime() - motor_issued_time >= motor_wait_time) {
+      analogWrite(MOTOR_R_PWM_PIN, 0);
+      analogWrite(MOTOR_L_PWM_PIN, 0);
+      motor_busy = false;
+      motor_wait_time = 0;
+    } else {
+      switch (motor_direction) {
+        case MotorDirection::Right:
+          analogWrite(MOTOR_R_PWM_PIN, 128);
+          analogWrite(MOTOR_L_PWM_PIN, 0);
+          break;
+        case MotorDirection::Left:
+          analogWrite(MOTOR_R_PWM_PIN, 0);
+          analogWrite(MOTOR_L_PWM_PIN, 128);
+          break;
+      }
+    }
+  }
 }
 
 // ---- CURTAIN CODE ----
 
 void curtain_reset() {
-  motor_right(5000);
+  issue_motor(MotorDirection::Right, 7);
   curtainState = CurtainState::Open;
 }
 
@@ -328,7 +261,7 @@ void curtain_open() {
   if (curtainState != CurtainState::Open) {
     lastCurtainState = curtainState;
     curtainState = CurtainState::Open;
-    motor_right(5000);
+    issue_motor(MotorDirection::Right, 7);
   }
 }
 
@@ -336,40 +269,43 @@ void curtain_close() {
   if (curtainState != CurtainState::Close) {
     lastCurtainState = curtainState;
     curtainState = CurtainState::Close;
-    motor_left(5000);
+    issue_motor(MotorDirection::Left, 7);
   }
 }
 
 // ---- MAIN LOOP ----
 void loop() {
-  openCloseButton.update([](int state) {
-    if (state == 0) curtain_open(); else curtain_close();
-    Serial << "Button 1: " << state << endl;
-  });
-  button2.update([](int new_value) {
-    Serial << "Button 2: " << new_value << endl;
-  });
-  button3.update([](int new_value) {
-    Serial << "Button 3: " << new_value << endl;
-  });
-  button4.update([](int new_value) {
-    Serial << "Button 4: " << new_value << endl;
-  });
-  button5.update([](int new_value) {
-    Serial << "Button 5: " << new_value << endl;
-  });
-  button6.update([](int new_value) {
-    Serial << "Button 6: " << new_value << endl;
-  });
-  button7.update([](int new_value) {
-    Serial << "Button 7: " << new_value << endl;
-  });
-  button8.update([](int new_value) {
-    Serial << "Button 8: " << new_value << endl;
-  });
-  button9.update([](int new_value) {
-    Serial << "Button 9: " << new_value << endl;
-  });
+  if (!motor_busy) {
+    openCloseButton.update([](int state) {
+      if (state == 0) curtain_open(); else curtain_close();
+    });
+    button2.update([](int new_value) {
+      Serial << "Button 2: " << new_value << endl;
+    });
+    button3.update([](int new_value) {
+      Serial << "Button 3: " << new_value << endl;
+    });
+    button4.update([](int new_value) {
+      Serial << "Button 4: " << new_value << endl;
+    });
+    button5.update([](int new_value) {
+      Serial << "Button 5: " << new_value << endl;
+    });
+    button6.update([](int new_value) {
+      Serial << "Button 6: " << new_value << endl;
+    });
+    button7.update([](int new_value) {
+      Serial << "Button 7: " << new_value << endl;
+    });
+    button8.update([](int new_value) {
+      Serial << "Button 8: " << new_value << endl;
+    });
+    setCurrentTimeButton.update([](int new_value) {
+      Serial << "set Current time: " << new_value << endl;
+    });
+  }
+  motor_update();
   display_time();
   display_state();
+  delay(10);
 }
